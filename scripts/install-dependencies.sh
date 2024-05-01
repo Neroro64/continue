62ba5dfd744e9a781b7de0da7cbbe6db517ab23b
NuoC <nuoc@abyssalminds.com>
2024-05-01 21:00:12 +0200

    Change npm to bun in the tasks

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -7,37 +7,27 @@ set -e
 
 echo "Installing Core extension dependencies..."
 pushd core
-npm install
-npm link
-
+bun install  
+bun link
 popd
 
 echo "Installing GUI extension dependencies..."
 pushd gui
-npm install
-npm link @continuedev/core
-npm run build
-
+bun install  
+bun link @continuedev/core
+bun run build
 popd
 
 # VSCode Extension (will also package GUI)
 echo "Installing VSCode extension dependencies..."
 pushd extensions/vscode
 # This does way too many things inline but is the common denominator between many of the scripts
-npm install
-npm link @continuedev/core
-npm run prepackage
-npm run package
+bun install  
+bun link @continuedev/core
+bun run package
 
 popd
 
 echo "Installing binary dependencies..."
 pushd binary
-npm install
-npm run build
-
-popd
-
-echo "Installing docs dependencies..."
-pushd docs
-npm install
+bun install  
